6f8a6985860a6365ccf31c9590436d6e71b97557
Diogo Ferreira Saucedo <Diogoferreiradfs@hotmail.com>
2022-12-29 14:22:13 -0400

    initial commit
    The base of the project was built. The necessary dependencies for connecting
    to the database (mariadb) were added, as well as the creation of environment
    variables. 'commitizen' was added to standardize commits.

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config/dist';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 3306)),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', '123456'),
+        database: configService.get('DB_DATABASE', 'todo'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
