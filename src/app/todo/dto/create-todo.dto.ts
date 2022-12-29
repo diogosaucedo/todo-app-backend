271fb75326f19d91dee66633814e50f9aec81fd2
Diogo Ferreira Saucedo <Diogoferreiradfs@hotmail.com>
2022-12-29 19:44:43 -0400

    feat: add documentation with swagger

--- src/app/todo/dto/create-todo.dto.ts
+++ src/app/todo/dto/create-todo.dto.ts
@@ -1,10 +1,16 @@
+import {
+  ApiProperty,
+  ApiPropertyOptional,
+} from '@nestjs/swagger/dist/decorators';
 import { IsIn, IsNotEmpty } from 'class-validator';
 
 export class CreateTodoDto {
   @IsNotEmpty()
+  @ApiProperty()
   task: string;
 
   @IsNotEmpty()
   @IsIn([0, 1])
+  @ApiPropertyOptional()
   isDone: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TODOApp API')
+    .setDescription('A to-do list API')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
